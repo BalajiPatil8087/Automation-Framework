082c1e58cefc137fce86a784425d1683e9b789fa
patilbs <patilsagar80878@gmail.com>
2023-01-04 16:51:32 +0530

    Demo on TestcaseSource

---
+++ EmployeeManagement/DemoTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    public  class DemoTest
+    {
+        public static object[] DemoValidData()
+        {
+            string[] dataset1 = new string[2];
+            dataset1[0] = "John";
+            dataset1[1] = "John123";
+
+            string[] dataset2 = new string[2];
+            dataset1[0] = "peter";
+            dataset1[1] = "peter123";
+
+            string[] dataset3 = new string[2];
+            dataset1[0] = "saul";
+            dataset1[1] = "saul123";
+
+            object[] alldataset=new object[3];//no of testcase
+
+            alldataset[0] = dataset1;
+            alldataset[1] = dataset2;
+            alldataset[2] = dataset3;
+
+            return alldataset;
+        }
+        [Test,TestCaseSource(nameof(DemoValidData))]
+        public void DemoValidLogin(string username, string password)
+        {
+            Console.WriteLine("Hello " + username + password);
+        }
+    }
+}
